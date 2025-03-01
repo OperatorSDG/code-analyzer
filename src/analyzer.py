9b051fa69613ca8c53e976ac23f8fddb922b4684
OperatorSDG <sourojeet.dasgupta2024@gmail.com>
2025-03-01 20:25:55 +0530

    added test

--- src/analyzer.py
+++ src/analyzer.py
@@ -1,2 +1,8 @@
-def analyze_code():
-    return "Code analysis not implemented yet!"
+import timeit
+
+def analyze_code(code_str: str):
+    try:
+        exec_time = timeit.timeit(code_str, number=1000)
+        return {"execution_time": exec_time}
+    except Exception as e:
+        return {"error": str(e)}
